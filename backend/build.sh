f265e6ddf125a1f1fbf3ae82fb97be2b6ca12faa
Yaroslav Papeshin <papeshin15@gmail.com>
2026-05-27 20:26:39 +0800

    fix(back): replace docker-compose to docker compose

--- backend/build.sh
+++ backend/build.sh
@@ -5,4 +5,4 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR"
 
 echo "Building Rust binary..."
-docker-compose -f docker-compose.build.yaml run --rm builder
+docker compose -f docker-compose.build.yaml run --rm builder
